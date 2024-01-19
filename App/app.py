a6cb672e9f700417ca3220bdec2a6720ef2ee360
TRAN THANH SON <91709267+FOX2920@users.noreply.github.com>
2024-01-19 17:14:15 +0700

    Update app.py

--- App/app.py
+++ App/app.py
@@ -21,15 +21,15 @@ if uploaded_file is not None:
     label_counts = df['label_id'].value_counts()
     
     # Hiển thị số lượng free_text của mỗi nhãn
-    st.write("Số lượng nhãn HATE (2):", label_counts.get(2, 0))
-    st.write("Số lượng nhãn CLEAN (0):", label_counts.get(0, 0))
+    st.write("Số lượng nhãn OFFENSIVE (2):", label_counts.get(2, 0))
+    st.write("Số lượng nhãn Clean (0):", label_counts.get(0, 0))
     st.write("Số lượng nhãn OFFENSIVE (1):", label_counts.get(1, 0))
 
     # Hiển thị biểu đồ thống kê
     st.subheader("Biểu đồ thống kê")
-    plt.figure(figsize=(8, 5))
-    sns.countplot(x='label_id', data=df)
-    st.pyplot()
+    fig, ax = plt.subplots(figsize=(8, 5))
+    sns.countplot(x='label_id', data=df, ax=ax)
+    st.pyplot(fig)
 
 else:
     st.warning("Vui lòng upload file CSV.")
